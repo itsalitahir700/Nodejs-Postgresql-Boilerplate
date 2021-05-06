2c2be19c28b5afa69ace5595a70b93335426d5ce
jmathins <jmathins@aislerocket.com>
2021-05-06 15:05:09 +0500

    1st commit to dev

--- server/modules/users/routes.js
+++ server/modules/users/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { authenticateToken } from "../utils/authenticate";
 import * as UserController from "./controller";
 const bodyParser = require("body-parser").json();
 const routes = new Router();
 
+routes.get("/", authenticateToken, UserController.welcome);
 routes.get("/users", UserController.getAllUsers);
 routes.get("/users/:id", UserController.getUserById);
 routes.post("/users", bodyParser, UserController.createUser);
